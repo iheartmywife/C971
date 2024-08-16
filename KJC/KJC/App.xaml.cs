9d4e3dca9105656ea24b4888e25a6b280a439d96
Kyle Chermak <kchermak@wgu.edu>
2024-08-16 11:19:19 -0700

    created initial project

---
+++ KJC/KJC/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KJC
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ KJC/KJC/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KJC
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
